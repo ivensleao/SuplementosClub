56ba6cd4da61757ba28a39c848d6d404a879c0bd
ivensleao <ivensleao@gmail.com>
2015-02-03 00:51:37 -0200

    Acertos dashboard e login

--- js/Pages/login.js
+++ js/Pages/login.js
@@ -1,1 +1,15 @@
-console.log("bla")
+$("#btnLogin").click(function() {
+	var login = $("#txtLogin").val();
+	var password = $("#txtPassword").val();
+ 	
+ 	if((login == "" || login == null || login  != "admin") || (password == "" || password == null || password != "teste123")){
+ 			$("#divError").removeClass('hide');
+ 			$("#divError").addClass('show');
+ 	}
+ 	else{   
+ 			$("#divError").removeClass('show');
+ 			$("#divError").addClass('hide'); 	
+
+ 			window.location.href='dashboard.html';		
+ 	}
+});
